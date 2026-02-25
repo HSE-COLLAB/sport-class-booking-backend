42fb1b51f136f96022d2e3498d28a14cc5222604
Ilya Kravchenko <ikravchenko@phoenixit.ru>
2026-02-25 17:15:01 +0300

    Initial commit

---
+++ src/main/java/ru/hse/sportclassbookingbackend/SportClassBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.sportclassbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportClassBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportClassBookingBackendApplication.class, args);
+    }
+
+}
