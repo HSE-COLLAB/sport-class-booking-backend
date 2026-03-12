1eee6cac4506fa7067a71577b13a4a6a08e65dc8
Ilya Kravchenko <ikravchenko@phoenixit.ru>
2026-03-12 23:14:50 +0300

    feat: change local db port and sql scripts

--- src/main/resources/db/changelog/changes/001-init/sql/001_create_health_groups.sql
+++ src/main/resources/db/changelog/changes/001-init/sql/001_create_health_groups.sql
@@ -1,5 +1,6 @@
 CREATE TABLE health_groups
 (
-    id INTEGER NOT NULL,
+    id          INTEGER      NOT NULL,
     description VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_health_groups PRIMARY KEY (id));
+    CONSTRAINT pk_health_groups PRIMARY KEY (id)
+);
--- src/main/resources/db/changelog/changes/001-init/sql/006_create_workout_types.sql
+++ src/main/resources/db/changelog/changes/001-init/sql/006_create_workout_types.sql
@@ -1,8 +1,9 @@
-CREATE TABLE workout_types (
-    id UUID NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    allow_health_group_id INTEGER NOT NULL,
-    is_active bool NOT NULL DEFAULT true,
+CREATE TABLE workout_types
+(
+    id                    UUID         NOT NULL,
+    title                 VARCHAR(255) NOT NULL,
+    allow_health_group_id INTEGER      NOT NULL,
+    is_active             BOOLEAN      NOT NULL DEFAULT true,
     CONSTRAINT pk_workout_types PRIMARY KEY (id),
-    CONSTRAINT fk_workout_types_health_groups FOREIGN KEY (allow_health_group_id) REFERENCES health_groups(id)
+    CONSTRAINT fk_workout_types_health_groups FOREIGN KEY (allow_health_group_id) REFERENCES health_groups (id)
 );
