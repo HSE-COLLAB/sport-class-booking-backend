fd356c6292be641e92552ad8f93c61fa5feb0f74
Nikita <mikheev_n@outlook.com>
2026-03-19 10:11:21 +0300

    fix: added service implementation, different minor mistakes corrected

--- src/main/java/ru/hse/sportclassbookingbackend/repository/HealthGroupRepository.java
+++ src/main/java/ru/hse/sportclassbookingbackend/repository/HealthGroupRepository.java
@@ -1,8 +1,6 @@
 package ru.hse.sportclassbookingbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.hse.sportclassbookingbackend.model.HealthGroup;
 
-@Repository
 public interface HealthGroupRepository extends JpaRepository<HealthGroup, Integer> {}
